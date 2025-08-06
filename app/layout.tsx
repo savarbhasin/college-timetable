cf155102792842e15ae89b54a571cde171be0c67
S <51899822+savarbhasin@users.noreply.github.com>
2025-08-06 18:34:18 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="dark">
       <body className="min-h-screen bg-background text-foreground">
-        <main className="container mx-auto p-6 max-w-7xl">{children}</main>
+        <main className="container mx-auto p-6 max-w-7xl">{children}
+         <Analytics />
+       </main>
       </body>
     </html>
   );
